e99b17bddd4c63ca6650f2fd3339feef422679e6
Masoumeh Fazlali <m.fazlali1983@gmail.com>
2022-07-25 16:17:52 +0200

    add if statement to html file

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def projects(request):
-    return render(request, './projects/projects.html',{'message':50})
+    page='projects'
+    number=12
+    context={'page':page,'number':number}
+    return render(request, './projects/projects.html',context)
 def project(request,pk):
-    return render(request, './projects/single-project.html')
+    return render(request, './projects/single-project.html',{'num':pk})
