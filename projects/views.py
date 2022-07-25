9333b55a98b83309b5360774d805dcc42be35cd6
Masoumeh Fazlali <m.fazlali1983@gmail.com>
2022-07-25 16:43:59 +0200

    add for statement to html file

--- projects/views.py
+++ projects/views.py
@@ -2,10 +2,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+projectslist=[
+    {'id':1,
+      'title':'ecommerce website',
+     'desc':'fully functional ecommerce website'},
+    {'id':2,
+      'title':'portfolio website',
+     'desc':'my portfolio'},
+    {'id':3,
+      'title':'social network',
+     'desc':'a sample network'}
+]
 def projects(request):
     page='projects'
     number=12
-    context={'page':page,'number':number}
+    context={'page':page,'number':number,'projects':projectslist}
     return render(request, './projects/projects.html',context)
 def project(request,pk):
     return render(request, './projects/single-project.html',{'num':pk})
