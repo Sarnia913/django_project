95461dd2cf9fa60728ca0286924c4d2f4fe10a51
Masoumeh Fazlali <m.fazlali1983@gmail.com>
2022-07-29 12:25:22 +0200

    create url and view function to create project form

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import project,projects
+from .views import project,projects,create_project
 
 urlpatterns=[
 
     path('',projects,name='projects'),
-    path('what/<str:pk>/',project,name='project')
+    path('what/<str:pk>/',project,name='project'),
+    path('create_project_form/', create_project, name='create_project')
       ]
--- projects/views.py
+++ projects/views.py
@@ -1,27 +1,20 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 # Create your views here.
-projectslist=[
-    {'id':1,
-      'title':'ecommerce website',
-     'desc':'fully functional ecommerce website'},
-    {'id':2,
-      'title':'portfolio website',
-     'desc':'my portfolio'},
-    {'id':3,
-      'title':'social network',
-     'desc':'a sample network'}
-]
+
 def projects(request):
-    page='projects'
-    number=12
-    context={'page':page,'number':number,'projects':projectslist}
+    projects=Project.objects.all()
+    context={'projects':projects}
     return render(request, './projects/projects.html',context)
 def project(request,pk):
-    projectobj=None
-    for proj in projectslist:
-        if proj['id']==int(pk):
-            projectobj=proj
-    context={'num':pk,'project':projectobj}
+    projectobj=Project.objects.get(id=pk)
+
+    context={'num':pk,'projectobj':projectobj}
     return render(request, './projects/single-project.html',context)
+def create_project(request):
+    form=ProjectForm()
+    context={'form':form}
+    return render(request,'./projects/project_form.html',context)
