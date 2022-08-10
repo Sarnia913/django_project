486ac72a8ba07e04f973b8698108f5fabefe7c8b
Masoumeh Fazlali <m.fazlali1983@gmail.com>
2022-08-10 10:47:04 +0200

    edit form module to have a nice view

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,widgets
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model=Project
         fields='__all__'
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
